efa1f7d756ea9338c99df10028941fdb8c2daaea
Feng Chen.Philip <fchen14@lsu.edu>
2018-06-25 22:22:52 -0500

    added float to int example

--- type_casting.c
+++ type_casting.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 main() {
    int a = 4, b = 3;
-   float c;
+   float c, d = 3.1415;
    c =  a / b;
    /* make sure you are doing the right conversion */
    printf("c= %f\n", c );
    c =  b / a;
    printf("c= %f\n", c );
    c = (float)a / b;
+   printf("c= %f\n", c );
+   c = (int)d;
    printf("c= %f\n", c );
 }
